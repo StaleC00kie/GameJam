4b2447ccf2ee3bbffd5edf846e95c66122463ea7
JamesBakerAIE <s191482@students.aie.edu.au>
2022-06-18 18:25:46 +0930

    Added Player WASD movement Added Player gravity

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    private PlayerController _cc;
+
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
